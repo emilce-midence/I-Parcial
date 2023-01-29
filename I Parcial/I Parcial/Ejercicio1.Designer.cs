527231668eda3c641b6ccc13f16613cf16fa688c
emilcemidence <emmc0798@gmail.com>
2023-01-28 21:58:39 -0600

    Segundo Commit
    Segundo Commit del repositorio

---
+++ I Parcial/I Parcial/Ejercicio1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace I_Parcial
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero1textBox = new System.Windows.Forms.TextBox();
+            this.Numero2textBox = new System.Windows.Forms.TextBox();
+            this.Sumarbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2:";
+            // 
+            // Numero1textBox
+            // 
+            this.Numero1textBox.Location = new System.Drawing.Point(182, 101);
+            this.Numero1textBox.Name = "Numero1textBox";
+            this.Numero1textBox.Size = new System.Drawing.Size(100, 20);
+            this.Numero1textBox.TabIndex = 2;
+            // 
+            // Numero2textBox
+            // 
+            this.Numero2textBox.Location = new System.Drawing.Point(182, 138);
+            this.Numero2textBox.Name = "Numero2textBox";
+            this.Numero2textBox.Size = new System.Drawing.Size(100, 20);
+            this.Numero2textBox.TabIndex = 3;
+            // 
+            // Sumarbutton
+            // 
+            this.Sumarbutton.Location = new System.Drawing.Point(192, 183);
+            this.Sumarbutton.Name = "Sumarbutton";
+            this.Sumarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Sumarbutton.TabIndex = 4;
+            this.Sumarbutton.Text = "Sumar";
+            this.Sumarbutton.UseVisualStyleBackColor = true;
+            this.Sumarbutton.Click += new System.EventHandler(this.Sumarbutton_Click);
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Sumarbutton);
+            this.Controls.Add(this.Numero2textBox);
+            this.Controls.Add(this.Numero1textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio1";
+            this.Text = "Numero 1:";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Numero1textBox;
+        private System.Windows.Forms.TextBox Numero2textBox;
+        private System.Windows.Forms.Button Sumarbutton;
+    }
+}
+
--- I Parcial/I Parcial/Ejercicio1.cs
+++ I Parcial/I Parcial/Ejercicio1.cs
@@ -9,11 +9,22 @@ using System.Windows.Forms;
 
 namespace I_Parcial
 {
-    public partial class Form1 : Form
+    public partial class Ejercicio1 : Form
     {
-        public Form1()
+        public Ejercicio1()
         {
             InitializeComponent();
+        }
+
+        private void Sumarbutton_Click(object sender, EventArgs e)
+        {
+            //Variables
+            decimal numero1 = Convert.ToDecimal(Numero1textBox.Text);
+            decimal numero2 = Convert.ToDecimal(Numero2textBox.Text);
+
+            decimal resultado = numero1 + numero2;
+
+            MessageBox.Show("La suma es: " + resultado);
         }
     }
 }
